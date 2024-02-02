fc08bcc3f808333194bca69de182e11894e12093
Tiana-Finaritra <128024026+Tiana-Finaritra@users.noreply.github.com>
2024-02-02 17:41:18 +0300

    feat: Hello word and pingPong test in controller

--- src/main/java/com/food_orders/FoodOrdersApplication.java
+++ src/main/java/com/food_orders/FoodOrdersApplication.java
@@ -1,4 +1,4 @@
-package com.food_orders.food_orders;
+package com.food_orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/food_orders/entity/FoodOrdersApplicationTests.java
+++ src/test/java/com/food_orders/entity/FoodOrdersApplicationTests.java
@@ -1,4 +1,4 @@
-package com.food_orders.food_orders;
+package com.food_orders.entity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
