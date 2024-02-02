58e44269e501160db3ecea685ab5b1b4ad2e7b88
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-02-02 14:58:37 +0300

    fix: change ids' types to UUID autoIncrement

--- database/autoRun.sql
+++ database/autoRun.sql
@@ -1,49 +1,83 @@
-\c postgres
+    \c postgres
 
-CREATE DATABASE paradish;
+    CREATE DATABASE paradish;
 
-\c paradish
+    \c paradish
 
-CREATE TABLE IF NOT EXISTS "User" (
-    id VARCHAR(250) PRIMARY KEY,
-    name VARCHAR(250) NOT NULL,
-    first_name VARCHAR(250),
-    sex CHAR(1) CHECK (sex = 'M' OR sex='F'),
-    email VARCHAR(250) NOT NULL UNIQUE,
-    password VARCHAR(250),
-    image TEXT,
-    is_admin BOOL,
-    residence VARCHAR(250)
-);
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS "Order" (
-    id VARCHAR(250) PRIMARY KEY,
-    date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    shipping_cost DOUBLE PRECISION,
-    delivery_date_time TIMESTAMP NOT NULL,
-    delivery_place VARCHAR(250) NOT NULL,
-    id_User VARCHAR(250) REFERENCES "User"(id)
-);
+    CREATE TABLE IF NOT EXISTS "User" (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(250) NOT NULL,
+        first_name VARCHAR(250),
+        sex CHAR(1) CHECK (sex = 'M' OR sex='F'),
+        email VARCHAR(250) NOT NULL UNIQUE,
+        password VARCHAR(250),
+        image TEXT,
+        is_admin BOOL,
+        residence VARCHAR(250)
+    );
 
-CREATE TABLE IF NOT EXISTS "Dish" (
-    id VARCHAR(250) PRIMARY KEY,
-    name VARCHAR(250) NOT NULL,
-    price DOUBLE PRECISION NOT NULL
-);
+    CREATE TABLE IF NOT EXISTS "Order" (
+        id UUID DEFAULT uuid_generate_v4 PRIMARY KEY,
+        date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        shipping_cost DOUBLE PRECISION,
+        delivery_date_time TIMESTAMP NOT NULL,
+        delivery_place VARCHAR(250) NOT NULL,
+        id_User UUID REFERENCES "User"(id)
+    );
 
-CREATE TABLE IF NOT EXISTS "Ingredient" (
-    id VARCHAR(250) PRIMARY KEY,
-    name VARCHAR(250) NOT NULL
-);
+    CREATE TABLE IF NOT EXISTS "Order" (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        shipping_cost DOUBLE PRECISION,
+        delivery_date_time TIMESTAMP NOT NULL,
+        delivery_place VARCHAR(250) NOT NULL,
+        id_User UUID REFERENCES "User"(id)
+    );
 
-CREATE TABLE IF NOT EXISTS "included" (
-    id VARCHAR(250) PRIMARY KEY,
-    id_Order VARCHAR(250) REFERENCES "Order"(id),
-    id_Dish VARCHAR(250) REFERENCES "Dish"(id)
-);
+    CREATE TABLE IF NOT EXISTS "Dish" (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(250) NOT NULL,
+        price DOUBLE PRECISION NOT NULL
+    );
 
-CREATE TABLE IF NOT EXISTS "contains" (
-    id VARCHAR(250) PRIMARY KEY,
-    id_Dish VARCHAR(250) REFERENCES "Dish"(id),
-    id_Ingredient VARCHAR(250) REFERENCES "Ingredient"(id)
+    CREATE TABLE IF NOT EXISTS "Ingredient" (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(250) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS "included" (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        id_Order UUID REFERENCES "Order"(id),
+        id_Dish UUID REFERENCES "Dish"(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS "contains" (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        id_Dish UUID REFERENCES "Dish"(id),
+        id_Ingredient UUID REFERENCES "Ingredient"(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS "Dish" (
+        id UUID DEFAULT uuid_generate_v4 PRIMARY KEY,
+        name VARCHAR(250) NOT NULL,
+        price DOUBLE PRECISION NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS "Ingredient" (
+        id UUID DEFAULT uuid_generate_v4 PRIMARY KEY,
+        name VARCHAR(250) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS "included" (
+        id UUID DEFAULT uuid_generate_v4 PRIMARY KEY,
+        id_Order UUID REFERENCES "Order"(id),
+        id_Dish UUID REFERENCES "Dish"(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS "contains" (
+        id UUID DEFAULT uuid_generate_v4 PRIMARY KEY,
+        id_Dish VARCHAR(250) REFERENCES "Dish"(id),
+        id_Ingredient UUID REFERENCES "Ingredient"(id)
 );
