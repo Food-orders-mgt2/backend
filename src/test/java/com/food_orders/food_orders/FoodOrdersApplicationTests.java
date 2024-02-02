9e2a1e11a4b73ca81250a21a7377dc4bfb0cde8a
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-02-02 12:44:58 +0300

    initial commit

---
+++ src/test/java/com/food_orders/food_orders/FoodOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_orders.food_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
