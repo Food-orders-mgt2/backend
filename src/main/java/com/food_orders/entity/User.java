f3a58096aa0e74b1242b64840cd78bb062a7beb2
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-02-04 12:55:38 +0300

    feat: User model

--- src/main/java/com/food_orders/entity/User.java
+++ src/main/java/com/food_orders/entity/User.java
@@ -14,7 +14,19 @@ public class User {
     private String name;
     private String first_name;
     private String email;
+    private String password;
     private String image;
     private String role;
     private String residence;
+
+    public User(String name, String first_name, String email, String password, String image, String role, String residence) {
+        this.name = name;
+        this.first_name = first_name;
+        this.email = email;
+        this.password = password;
+        this.image = image;
+        this.role = role;
+    }
 }
+
+
