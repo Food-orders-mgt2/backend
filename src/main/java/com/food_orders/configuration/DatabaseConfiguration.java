342242a6400d16e371e9c3095d7ff777d7423c66
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-02-04 12:49:08 +0300

    chore: try to run project without env variabble

--- src/main/java/com/food_orders/configuration/DatabaseConfiguration.java
+++ src/main/java/com/food_orders/configuration/DatabaseConfiguration.java
@@ -1,19 +1,36 @@
 package com.food_orders.configuration;
 
+import com.food_orders.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+@Configuration
 public class DatabaseConfiguration {
+//
+//    @Value("${spring.datasource.url}")
+//    private String dbUrl;
+//
+//    @Value("${spring.datasource.username}")
+//    private String dbUsername;
+//
+//    @Value("${spring.datasource.password}")
+//
 
+    private String dbUrl = "jdbc:postgresql://localhost/paradish";
+    private String dbUsername = "postgres";
+    private String dbPassword = "12345678";
+
+    @Bean
     public Connection getConnection() throws SQLException {
-        String dbURL = System.getenv("dbURL");
-        String dbUSERNAME = System.getenv("dbUSERNAME");
-        String dbPASSWORD = System.getenv("dbPASSWORD");
+        return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+    }
 
-        System.out.println("dbURL : " + dbURL);
-        System.out.println("dbUSERNAME : " + dbUSERNAME);
-
-        return DriverManager.getConnection(dbURL, dbUSERNAME, dbPASSWORD);
+    @Bean
+    public UserDAO userDAO(Connection connection) {
+        return new UserDAO(connection);
     }
 }
