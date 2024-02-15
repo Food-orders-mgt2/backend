5cd20c042ae22ad2372797231f327a5eafaf57c9
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-15 19:00:36 +0100

    fix : Update configuration cors

--- src/main/java/com/food_orders/configuration/CorsConfig.java
+++ src/main/java/com/food_orders/configuration/CorsConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
@@ -12,8 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/dish")
-                .allowedOrigins(" http://localhost:5173", "https://main--para-dish.netlify.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:5173", "https://main--para-dish.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Origin", "Content-Type", "Accept");
     }
 }
+
