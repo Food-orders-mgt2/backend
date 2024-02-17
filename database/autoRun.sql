e1d9b3290c97c912593592667b4f380afca7e09d
junkaM <hei.mikanto.3@gmail.com>
2024-02-17 22:25:36 +0100

    fix: fix database structure &  entity

--- database/autoRun.sql
+++ database/autoRun.sql
@@ -1,54 +1,53 @@
-    \c postgres
+        \c postgres
 
-    DROP DATABASE paradish;
+        DROP DATABASE paradish;
 
-    CREATE DATABASE paradish;
+        CREATE DATABASE paradish;
 
-    \c paradish
+        \c paradish
 
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+        CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-    CREATE TABLE IF NOT EXISTS "User" (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(250) NOT NULL,
-        first_name VARCHAR(250),
-        email VARCHAR(250) NOT NULL UNIQUE,
-        password VARCHAR(250),
-        image TEXT,
-        role VARCHAR(250) CHECK (role = 'customer' OR role = 'admin'),
-        residence VARCHAR(250)
-    );
+        CREATE TABLE IF NOT EXISTS "User" (
+            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+            username VARCHAR(250) NOT NULL,
+            email VARCHAR(250) NOT NULL UNIQUE,
+            password VARCHAR(250)
+        );
 
-      CREATE TABLE IF NOT EXISTS "Order" (
-          id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-          date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-          shipping_cost DOUBLE PRECISION,
-          delivery_date_time TIMESTAMP NOT NULL,
-          delivery_place VARCHAR(250) NOT NULL,
-          id_User UUID REFERENCES "User"(id),
-          pay_mode VARCHAR(250) CHECK (pay_mode= 'Mobile money' OR pay_mode = 'Bank' OR pay_mode = 'Cash')
-      );
+          CREATE TABLE IF NOT EXISTS "Order" (
+              id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+              date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              shipping_cost DOUBLE PRECISION,
+              delivery_date_time TIMESTAMP NOT NULL,
+              delivery_address VARCHAR(250) NOT NULL,
+              list_dish_id VARCHAR(250),
+              id_User UUID REFERENCES "User"(id),
+              pay_mode VARCHAR(250) CHECK (pay_mode= 'Mobile money' OR pay_mode = 'Bank' OR pay_mode = 'Cash'),
+              total_price DOUBLE PRECISION
+          );
 
-    CREATE TABLE IF NOT EXISTS "Dish" (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(250) NOT NULL,
-        price DOUBLE PRECISION NOT NULL,
-        image VARCHAR(250)
-    );
+        CREATE TABLE IF NOT EXISTS "Dish" (
+            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+            name VARCHAR(250) NOT NULL,
+            description VARCHAR(250) NOT NULL,
+            price DOUBLE PRECISION NOT NULL,
+            image VARCHAR(250)
+        );
 
-    CREATE TABLE IF NOT EXISTS "Ingredient" (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(250) NOT NULL
-    );
+        CREATE TABLE IF NOT EXISTS "Ingredient" (
+            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+            name VARCHAR(250) NOT NULL
+        );
 
-    CREATE TABLE IF NOT EXISTS "included" (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        id_Order UUID REFERENCES "Order"(id),
-        id_Dish UUID REFERENCES "Dish"(id)
-    );
+        CREATE TABLE IF NOT EXISTS "included" (
+            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+            id_Order UUID REFERENCES "Order"(id),
+            id_Dish UUID REFERENCES "Dish"(id)
+        );
 
-    CREATE TABLE IF NOT EXISTS "contains" (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        id_Dish UUID REFERENCES "Dish"(id),
-        id_Ingredient UUID REFERENCES "Ingredient"(id)
-    );
+        CREATE TABLE IF NOT EXISTS "contains" (
+            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+            id_Dish UUID REFERENCES "Dish"(id),
+            id_Ingredient UUID REFERENCES "Ingredient"(id)
+        );
--- database/dummy_data.sql
+++ database/dummy_data.sql
@@ -1,48 +1,53 @@
 -- Insertions pour la table "User"
-INSERT INTO "User" (name, first_name,  email, password, image, role, residence) VALUES
-('Doe', 'John', 'john.doe@email.com', 'hashed_password_1', '/images/john.jpg', 'admin', 'City A'),
-('Smith', 'Alice', 'alice.smith@email.com', 'hashed_password_2', '/images/alice.jpg','admin', 'City B'),
-('Brown', 'Bob', 'bob.brown@email.com', 'hashed_password_3', '/images/bob.jpg','customer','City C'),
-('White', 'Eva', 'eva.white@email.com', 'hashed_password_4', '/images/eva.jpg','customer',  'City A'),
-('Johnson', 'Charlie', 'charlie.johnson@email.com', 'hashed_password_5', '/images/charlie.jpg','customer', 'City B');
+INSERT INTO "User" ( username, email, password)
+VALUES
+( 'john_doe', 'john.doe@email.com', 'hashed_password_1'),
+( 'alice_smith', 'alice.smith@email.com', 'hashed_password_2'),
+( 'bob_brown', 'bob.brown@email.com', 'hashed_password_3'),
+( 'eva_white', 'eva.white@email.com', 'hashed_password_4'),
+( 'charlie_johnson', 'charlie.johnson@email.com', 'hashed_password_5');
 
 -- Insertions pour la table "Order"
-INSERT INTO "Order" (date_time, shipping_cost, delivery_date_time, delivery_place, id_User, pay_mode) VALUES
-('2024-02-02 12:00:00', 10.50, '2024-02-02 12:00:00', 'Address 1', (SELECT id FROM "User" WHERE name = 'Doe'), 'Mobile money'),
-('2024-02-03 14:30:00', 8.20, '2024-02-03 14:30:00', 'Address 2', (SELECT id FROM "User" WHERE name = 'Smith'), 'Mobile money'),
-('2024-02-04 18:45:00', 15.75, '2024-02-04 18:45:00', 'Address 3', (SELECT id FROM "User" WHERE name = 'Brown'), 'Bank'),
-('2024-02-05 11:15:00', 12.00, '2024-02-05 11:15:00', 'Address 4', (SELECT id FROM "User" WHERE name = 'White'), 'Cash'),
-('2024-02-06 16:00:00', 7.80, '2024-02-06 16:00:00', 'Address 5', (SELECT id FROM "User" WHERE name = 'Johnson'), 'Cash');
+INSERT INTO "Order" ( date_time, shipping_cost, delivery_date_time, delivery_address, list_dish_id, id_User, pay_mode, total_price)
+VALUES
+( CURRENT_TIMESTAMP, 10.50, '2024-02-02 12:00:00', 'Address 1', '1,2', (SELECT id FROM "User" WHERE username = 'john_doe'), 'Mobile money', 10.50),
+( CURRENT_TIMESTAMP, 8.20, '2024-02-03 14:30:00', 'Address 2', '3,4', (SELECT id FROM "User" WHERE username = 'alice_smith'), 'Mobile money', 8.20),
+( CURRENT_TIMESTAMP, 15.75, '2024-02-04 18:45:00', 'Address 3', '5', (SELECT id FROM "User" WHERE username = 'bob_brown'), 'Bank', 15.75),
+( CURRENT_TIMESTAMP, 12.00, '2024-02-05 11:15:00', 'Address 4', '6', (SELECT id FROM "User" WHERE username = 'eva_white'), 'Cash', 12.00),
+( CURRENT_TIMESTAMP, 7.80, '2024-02-06 16:00:00', 'Address 5', '7', (SELECT id FROM "User" WHERE username = 'charlie_johnson'), 'Cash', 7.80);
 
 -- Insertions pour la table "Dish"
-INSERT INTO "Dish" (name, price,image) VALUES
-('Pasta Carbonara', 12.99,'dnflkl/ildn.img'),
-('Chicken Alfredo', 15.50,'dnflkl/ildn.img'),
-('Margherita Pizza', 9.75,'dnflkl/ildn.img'),
-('Beef Stir-Fry', 14.25,'dnflkl/ildn.img'),
-('Vegetarian Lasagna', 11.99,'dnflkl/ildn.img');
+INSERT INTO "Dish" ( name, description, price, image)
+VALUES
+( 'Pasta Carbonara', 'Creamy pasta with bacon and parmesan', 12.99, 'https://drive.google.com/file/d/1OGRufInDko7TCWsSoh2NIaFRymZdjfw2/view?usp=drive_link'),
+( 'Chicken Alfredo', 'Creamy pasta with chicken and alfredo sauce', 15.50, 'https://drive.google.com/file/d/1ZYMer0wPAhwALrfOjiWJwJ720qA3fopM/view?usp=drive_link'),
+( 'Margherita Pizza', 'Classic pizza with tomato sauce and mozzarella cheese', 9.75, 'https://drive.google.com/file/d/1nUgJLMq4A0ozupo20AHCll374I-o5GfN/view?usp=drive_link'),
+( 'Asparagus and Beef Stir-Fry', 'Stir-fried beef with vegetables and soy sauce', 14.25, 'https://drive.google.com/file/d/1Uy89AB1lqvmZBkihqMTZ1KZJc1PgDtW7/view?usp=drive_link'),
+( 'Vegetarian Lasagna', 'Layered pasta with vegetables and cheese', 11.99, 'https://drive.google.com/file/d/1s9huyTaWXUQkbAIBVziRgRUYhT0dGo0C/view?usp=drive_link');
 
 -- Insertions pour la table "Ingredient"
-INSERT INTO "Ingredient" (name) VALUES
-('Spaghetti'),
-('Chicken Breast'),
-('Tomato Sauce'),
-('Beef Strips'),
-('Lasagna Noodles');
+INSERT INTO "Ingredient" ( name)
+VALUES
+( 'Spaghetti'),
+( 'Chicken Breast'),
+( 'Tomato Sauce'),
+( 'Beef Strips'),
+( 'Lasagna Noodles');
 
 -- Insertions pour la table "included"
-INSERT INTO "included" (id_Order, id_Dish) VALUES
-((SELECT id FROM "Order" WHERE shipping_cost = 10.50), (SELECT id FROM "Dish" WHERE name = 'Pasta Carbonara')),
-((SELECT id FROM "Order" WHERE shipping_cost = 8.20), (SELECT id FROM "Dish" WHERE name = 'Chicken Alfredo')),
-((SELECT id FROM "Order" WHERE shipping_cost = 15.75), (SELECT id FROM "Dish" WHERE name = 'Margherita Pizza')),
-((SELECT id FROM "Order" WHERE shipping_cost = 12.00), (SELECT id FROM "Dish" WHERE name = 'Beef Stir-Fry')),
-((SELECT id FROM "Order" WHERE shipping_cost = 7.80), (SELECT id FROM "Dish" WHERE name = 'Vegetarian Lasagna'));
-
+INSERT INTO "included" ( id_Order, id_Dish)
+VALUES
+( (SELECT id FROM "Order" WHERE shipping_cost = 10.50), (SELECT id FROM "Dish" WHERE name = 'Pasta Carbonara')),
+( (SELECT id FROM "Order" WHERE shipping_cost = 8.20), (SELECT id FROM "Dish" WHERE name = 'Chicken Alfredo')),
+( (SELECT id FROM "Order" WHERE shipping_cost = 15.75), (SELECT id FROM "Dish" WHERE name = 'Margherita Pizza')),
+( (SELECT id FROM "Order" WHERE shipping_cost = 12.00), (SELECT id FROM "Dish" WHERE name = 'Asparagus and Beef Stir-Fry')),
+( (SELECT id FROM "Order" WHERE shipping_cost = 7.80), (SELECT id FROM "Dish" WHERE name = 'Vegetarian Lasagna'));
 
 -- Insertions pour la table "contains"
-INSERT INTO "contains" (id_Dish, id_Ingredient) VALUES
-((SELECT id FROM "Dish" WHERE name = 'Pasta Carbonara'), (SELECT id FROM "Ingredient" WHERE name = 'Spaghetti')),
-((SELECT id FROM "Dish" WHERE name = 'Chicken Alfredo'), (SELECT id FROM "Ingredient" WHERE name = 'Chicken Breast')),
-((SELECT id FROM "Dish" WHERE name = 'Margherita Pizza'), (SELECT id FROM "Ingredient" WHERE name = 'Tomato Sauce')),
-((SELECT id FROM "Dish" WHERE name = 'Beef Stir-Fry'), (SELECT id FROM "Ingredient" WHERE name = 'Beef Strips')),
-((SELECT id FROM "Dish" WHERE name = 'Vegetarian Lasagna'), (SELECT id FROM "Ingredient" WHERE name = 'Lasagna Noodles'));
+INSERT INTO "contains" ( id_Dish, id_Ingredient)
+VALUES
+( (SELECT id FROM "Dish" WHERE name = 'Pasta Carbonara'), (SELECT id FROM "Ingredient" WHERE name = 'Spaghetti')),
+( (SELECT id FROM "Dish" WHERE name = 'Chicken Alfredo'), (SELECT id FROM "Ingredient" WHERE name = 'Chicken Breast')),
+( (SELECT id FROM "Dish" WHERE name = 'Margherita Pizza'), (SELECT id FROM "Ingredient" WHERE name = 'Tomato Sauce')),
+( (SELECT id FROM "Dish" WHERE name = 'Beef Stir-Fry'), (SELECT id FROM "Ingredient" WHERE name = 'Beef Strips')),
+( (SELECT id FROM "Dish" WHERE name = 'Vegetarian Lasagna'), (SELECT id FROM "Ingredient" WHERE name = 'Lasagna Noodles'));
