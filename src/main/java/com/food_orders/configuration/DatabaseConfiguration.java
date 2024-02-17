b20f0a4391797c537b0e476b69e6ed1c56ef1179
Zo-Tsilavina <hei.tsilavina@gmail.com>
2024-02-17 18:48:42 +0100

    fix: add cors config into database configuration

--- src/main/java/com/food_orders/configuration/DatabaseConfiguration.java
+++ src/main/java/com/food_orders/configuration/DatabaseConfiguration.java
@@ -3,12 +3,15 @@ package com.food_orders.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @Configuration
-public class DatabaseConfiguration {
+public class DatabaseConfiguration implements WebMvcConfigurer {
 
     @Value("${spring.datasource.url}")
     private String url;
@@ -18,6 +21,14 @@ public class DatabaseConfiguration {
 
     @Value("${spring.datasource.password}")
     private String password;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://main--para-dish.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 
     @Bean
     public Connection getConnection() throws SQLException {
